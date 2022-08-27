2691c80c1d1f17a15d8e4ac2e739168b8b20c4c7
lkmelberg <100446631+lkmelberg@users.noreply.github.com>
2022-08-27 11:51:08 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,16 +1,21 @@
+// The index.html file links to products.js. Loop through the array in products.js and display each object using HTML.
+// Provide a default value for the price property if it is missing.
+// If the product is sold out, display its name in red otherwise display it in green.
+// Change the properties in the objects in the array to test your code.
+
 const products = [
-    {
-        name: "Fancy Product",
-        price: 40,
-        soldOut: true,
-    },
-    {
-        name: "Another Product",
-        soldOut: false,
-    },
-    {
-        name: "Cheap Product",
-        price: 10,
-        soldOut: true,
-    },
+  {
+    name: "Fancy Product",
+    price: 40,
+    soldOut: true,
+  },
+  {
+    name: "Another Product",
+    soldOut: false,
+  },
+  {
+    name: "Cheap Product",
+    price: 10,
+    soldOut: true,
+  },
 ];
